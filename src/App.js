b37b17df6f8ce0d94c41f58b977feb6d662bcaf2
İsmail Uğur Duyar <ismailugurduyar@gmail.com>
2020-07-04 01:59:44 +0300

    search bar added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import React, { useEffect } from 'react';
+import React, { Fragment, useEffect } from 'react';
+import SearchBar from './components/layout/SearchBar'
+
 
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
@@ -11,10 +13,10 @@ const App = () => {
     M.AutoInit();
   })
   return (
-    <div className="App">
-      My App
-    </div>
+    <Fragment>
+      <SearchBar/>
+    </Fragment>
   );
-}
+};
 
 export default App;
