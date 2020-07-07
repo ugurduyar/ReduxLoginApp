a7b46837165a0f6fed1442b05c0b8d3a84319393
İsmail Uğur Duyar <ismailugurduyar@gmail.com>
2020-07-07 04:26:56 +0300

    tech item added to the tech list modal

--- src/components/techs/TechListModal.js
+++ src/components/techs/TechListModal.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import AddTechModal from "./AddTechModal";
+import TechItem from "./TechItem";
 
 const TechListModal = () => {
   const [techs, setTechs] = useState([]);
@@ -25,9 +25,7 @@ const TechListModal = () => {
         <h4>Technician List</h4>
         <ul className="collection">
           {!loading &&
-            techs.map((tech) => (
-              <li className="collection-item">{tech.firstName}</li>
-            ))}
+            techs.map((tech) => <TechItem tech={tech} key={tech.id} />)}
         </ul>
       </div>
     </div>
