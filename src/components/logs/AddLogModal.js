bdee7efe1ade7a4f12bf03001057accf3fa3c6ea
İsmail Uğur Duyar <ismailugurduyar@gmail.com>
2020-07-10 05:25:53 +0300

    addlogmodal

--- src/components/logs/AddLogModal.js
+++ src/components/logs/AddLogModal.js
@@ -1,7 +1,10 @@
 import React, { useState } from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import { addLog } from "../../actions/logActions";
 import M from "materialize-css/dist/js/materialize.min.js";
 
-const AddLogModal = () => {
+const AddLogModal = ({ addLog }) => {
   const [message, setMessage] = useState("");
   const [attention, setAttention] = useState(false);
   const [tech, setTech] = useState("");
@@ -80,9 +83,13 @@ const AddLogModal = () => {
   );
 };
 
+AddLogModal.PropTypes = {
+  addLogL: PropTypes.func.isRequired,
+};
+
 const modalStyle = {
   width: "75%",
   height: "75%",
 };
 
-export default AddLogModal;
+export default connect(null, { addLog })(AddLogModal);
