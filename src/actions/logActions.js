c47c7e38102f82f2d0db7b5daf88f88070cb3375
İsmail Uğur Duyar <ismailugurduyar@gmail.com>
2020-07-10 06:22:47 +0300

    update-set-clear  data added

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -4,6 +4,9 @@ import {
   LOGS_ERROR,
   ADD_LOG,
   DELETE_LOG,
+  UPDATE_LOG,
+  SET_CURRENT,
+  CLEAR_CURRENT,
 } from "./types";
 
 // Get logs from server
@@ -68,6 +71,45 @@ export const deleteLog = (id) => async (dispatch) => {
       payload: err.response.data,
     });
   }
+};
+
+// Update log
+export const updateLog = (log) => async (dispatch) => {
+  try {
+    setLoading();
+    await fetch(`/logs/${id}`, {
+      method: "PUT",
+      body: JSON.stringify(log),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    dispatch({
+      type: UPDATE_LOG,
+      payload: data,
+    });
+  } catch (err) {
+    dispatch({
+      type: LOGS_ERROR,
+      payload: err.response.data,
+    });
+  }
+};
+
+// Set current log
+export const setCurrent = (log) => {
+  return {
+    type: SET_CURRENT,
+    payload: log,
+  };
+};
+
+// Clear current log
+export const clearCurrent = (log) => {
+  return {
+    type: CLEAR_CURRENT,
+  };
 };
 
 // Set loading to true
