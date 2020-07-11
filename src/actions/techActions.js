969ee549ae8ea8abf21e198b11c24b19213b25ca
İsmail Uğur Duyar <ismailugurduyar@gmail.com>
2020-07-11 04:32:48 +0300

    get techs and set loading added, log actions response fix

--- src/actions/techActions.js
+++ src/actions/techActions.js
@@ -7,3 +7,26 @@ import {
 } from "./types";
 
 // Get techs from server
+export const getTechs = () => async (dispatch) => {
+  try {
+    setLoading();
+    const res = await fetch("/techs");
+    const data = await res.json();
+
+    dispatch({
+      type: GET_TECHS,
+      payload: data,
+    });
+  } catch (err) {
+    dispatch({
+      type: TECHS_ERROR,
+      payload: err.response.statusText,
+    });
+  }
+};
+// Set loading to true
+export const setLoading = () => {
+  return {
+    type: SET_LOADING,
+  };
+};
--- src/reducers/techReducer.js
+++ src/reducers/techReducer.js
@@ -1,4 +1,10 @@
-import { defineLocale } from "moment";
+import {
+  GET_TECHS,
+  ADD_TECH,
+  DELETE_TECH,
+  SET_LOADING,
+  TECHS_ERROR,
+} from "../actions/types";
 
 const initialState = {
   techs: null,
@@ -8,6 +14,17 @@ const initialState = {
 
 export default (state = initialState, action) => {
   switch (action.type) {
+    case GET_TECHS:
+      return {
+        ...state,
+        techs: action.payload,
+        loading: false,
+      };
+    case SET_LOADING:
+      return {
+        ...state,
+        loading: true,
+      };
     default:
       return state;
   }
